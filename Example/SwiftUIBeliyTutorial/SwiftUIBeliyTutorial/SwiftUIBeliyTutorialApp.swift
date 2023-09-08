8b6aa368d12f376f68f6e8457dce8ef7e9a3df7c
Oleksandr Isaiev <nevkluchat@gmail.com>
2023-09-08 15:14:32 +0300

    append

---
+++ Example/SwiftUIBeliyTutorial/SwiftUIBeliyTutorial/SwiftUIBeliyTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBeliyTutorialApp.swift
+//  SwiftUIBeliyTutorial
+//
+//  Created by Oleksandr Isaiev on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBeliyTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
