5fe53ea07667e0cbc1b3c7d393badaf50ba09114
Oleksandr Isaiev <nevkluchat@gmail.com>
2023-09-08 09:18:03 +0300

    add empty example project

---
+++ Example/PokemonExample/PokemonExample/PokemonExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonExampleApp.swift
+//  PokemonExample
+//
+//  Created by Oleksandr Isaiev on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
